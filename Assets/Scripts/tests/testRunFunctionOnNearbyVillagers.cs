1e139f6eafe9efb0472802527cec767ce766d8db
syoels <syoels@gmail.com>
2018-01-26 01:12:02 +0200

    test affect villagers in radius around you

---
+++ Assets/Scripts/tests/testRunFunctionOnNearbyVillagers.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testRunFunctionOnNearbyVillagers : MonoBehaviour {
+
+
+    public float radius = 10f;
+
+    void Start(){
+        
+    }
+
+    void Update(){
+
+        if (Input.GetKeyDown(KeyCode.Space)){
+            testVillagerShowAffected[] villagers = FindObjectsOfType<testVillagerShowAffected>();
+            foreach (testVillagerShowAffected villager in villagers) {
+
+                if ((transform.position - villager.transform.position).magnitude <= radius) {
+                    villager.changeSpriteColor();
+                }
+
+            }
+        }
+
+    }
+
+
+
+
+    void OnDrawGizmosSelected() {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+}
---
+++ Assets/Scripts/tests/testVillagerShowAffected.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testVillagerShowAffected : MonoBehaviour {
+
+    private SpriteRenderer sr;
+    public int framesToColor = 24; 
+    private int framesToColorCounter = 0;
+    public Color origColor; 
+    public Color changeColor; 
+
+
+    // Use this for initialization
+    void Start () {
+        sr = GetComponentInChildren<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (framesToColorCounter > 0) {
+            framesToColorCounter--;
+        } else {
+            //sr.color = origColor;
+        }
+
+    }
+
+    // doesnt really color but never mind.. :)
+    public void changeSpriteColor(){
+        framesToColorCounter = framesToColor;
+        sr.color = changeColor;
+    }
+}
