9fd96c827f70eded96c59254a408b05c0cc83f20
syoels <syoels@gmail.com>
2018-01-25 23:26:58 +0200

    added test script (empty)

---
+++ Assets/Scripts/tests/testJump.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testJump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
