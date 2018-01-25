959041236b2ba95cde8733c032f0b93ec4aa5392
syoels <syoels@gmail.com>
2018-01-26 00:13:30 +0200

    jump test finished

--- Assets/Scripts/tests/testJump.cs
+++ Assets/Scripts/tests/testJump.cs
@@ -1,16 +1,52 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
+[RequireComponent(typeof(Rigidbody))]
 public class testJump : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float TOUCH_GROUND_THRESHOLD = 1f;
+    [SerializeField] private bool isGrounded;
+    public float jumpForce = 140f;
+
+    private bool jump = false;
+    private bool grounded = false;
+    private Rigidbody2D rb;
+
+
+
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update() {
+        this.ProcessPlayerInput();
+       
+    }
+
+    void FixedUpdate() {
+        this.HandleMovement();
+
+        
+    }
+
+    private void ProcessPlayerInput() {
+        float downY = transform.position.y - TOUCH_GROUND_THRESHOLD;
+        Vector3 endGroundCheck = new Vector3(transform.position.x, downY, transform.position.z);
+
+        Debug.DrawLine(transform.position, endGroundCheck, Color.red);
+
+        grounded = Physics2D.Linecast(transform.position, endGroundCheck, 1 << LayerMask.NameToLayer("Ground"));
+        if (Input.GetButtonDown("Jump") && grounded) {
+            jump = true;
+        }
+    }
+
+    private void HandleMovement() {
+        if (jump) {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
+    }
 }
