b1726fedf42903e94f2f7aab46b683cfbcf6e6ca
syoels <syoels@gmail.com>
2018-01-26 02:23:01 +0200

    cammera follow script

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target = null; 
+    [Range(0f, 1f)]
+    public float smoothing = 0.125f;
+    public Vector3 offset = new Vector3(1f, 1f, 0f);
+
+	// Use this for initialization. //TODO: Doesn't work when not assigned from inpesctor
+	void Start () {
+        if (target == null){
+            target = FindObjectOfType<PlayerController>().transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 targetPosition = target.position - offset;
+        Vector2 positionXY = Vector2.Lerp(transform.position, targetPosition, smoothing);
+        transform.position = new Vector3(positionXY.x, positionXY.y, -10f);
+	}
+}
