ccff50e03d860d6c5caba395c7435f11da95afd3
syoels <syoels@gmail.com>
2018-01-26 13:42:57 +0200

    added cntrol points script

--- Assets/Scripts/ControlPoint.cs
+++ Assets/Scripts/ControlPoint.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class ControlPoint : MonoBehaviour {
 
+
+    [System.Serializable]
+    public struct Instruction {
+        public Flamer target;
+        public  Vector3 velocity;
+    }
+    public Instruction[] mapping;
+
+    private Dictionary<int, Vector3> innerMapping = new Dictionary<int, Vector3>();
+
 	// Use this for initialization
 	void Start () {
-		
+        foreach (Instruction i in mapping) {
+            innerMapping.Add(i.target.GetInstanceID(), i.velocity);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public Vector3 getInstruction(int instanceID){
+        if(innerMapping.ContainsKey(instanceID)){
+            return innerMapping[instanceID];
+        } else {
+            return null;
+        }
+    }
 }
