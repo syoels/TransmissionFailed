2ebe65666c1430811e1dbf1ed9804d30580b3659
syoels <syoels@gmail.com>
2018-01-27 16:33:51 +0200

    button spites

--- Assets/Scripts/Intro.cs
+++ Assets/Scripts/Intro.cs
@@ -6,16 +6,39 @@ using UnityEngine.SceneManagement;
 
 public class Intro : MonoBehaviour {
 
-	public Button startBtn;
+//	public Button newGame;
+//    public Button next;
+//    public Button esc;
+
+
+    public GameObject blackPanel; 
+
+
 
 	void Start()
 	{
-		Button btn = startBtn.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
+        Debug.Log("In start");
+//		Button btnNew = newGame.GetComponent<Button>();
+//        Button btnEsc = esc.GetComponent<Button>();
+//        Button btnNxt = next.GetComponent<Button>();
+//        btnNew.onClick.AddListener(ShowComics);
+//        btnEsc.onClick.AddListener(HideComics);
+//        btnNxt.onClick.AddListener(StartGame);
 	}
 
-	void TaskOnClick()
-	{
+    public void ShowComics(){
+        Debug.Log("In ShowComics");
+        blackPanel.gameObject.SetActive(true);
+    }
+
+    public void HideComics(){
+        Debug.Log("In HideComics");
+        blackPanel.gameObject.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("In StartGame");
 		SceneManager.LoadScene ("Level_1");
 	}
 }
