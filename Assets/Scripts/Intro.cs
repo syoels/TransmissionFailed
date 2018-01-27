1c68253cadac9a5ebd0e7fbe859d0d37052292aa
Roy <roy@40nuggets.com>
2018-01-27 15:21:34 +0200

    add start scene

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour {
+
+	public Button startBtn;
+
+	void Start()
+	{
+		Button btn = startBtn.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		SceneManager.LoadScene ("Level_1");
+	}
+}
